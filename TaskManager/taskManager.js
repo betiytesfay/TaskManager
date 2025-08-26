2d81ed839eb49b4df81d7af454646737dd1b189c
Bethlehem Tesfay <betiytes@gmail.com>
2025-08-26 03:51:41 -0700

    done

--- TaskManager/taskManager.js
+++ TaskManager/taskManager.js
@@ -2,10 +2,10 @@ use taskManagerDB
 db.users.insertOne({ _id: 1, name: "Beti", email: "Beti@gmail.com" })
 db.users.insertOne({ _id: 2, name: "Selam", email: "Selam@gmail.com" })
 
-db.tasks.insertOne({ title: "Finish project", assignedTo: 1, status: "pending", tags: ["urgent"] })
-db.tasks.insertOne({ title: "Buy groceries", assignedTo: 2, status: "done", tags: ["personal"] })
+db.tasks.insertOne({ title: "Finish project", assignedTo: 1, status: "pending", tags: ["urgent", "personal"] })
+db.tasks.insertOne({ title: "Buy groceries", assignedTo: 2, status: "done", tags: ["personal", "for cooking"] })
 
-db.tasks.insertOne({ title: "Plan trip", assignedTo: 1, status: "in progress", tags: ["travel"] })
+db.tasks.insertOne({ title: "Plan trip", assignedTo: 1, status: "in progress", tags: ["travel", "passport"] })
 
 db.users.find()
 db.tasks.find({ assignedTo: 1 })
